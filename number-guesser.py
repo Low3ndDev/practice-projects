1e34fa2f84d83109c8ee48857af1908843ec380c
Low3ndDev <johnmark.betes@gmail.com>
2025-11-28 22:50:49 +0800

    Add guess counter with correct guess/es

--- number-guesser.py
+++ number-guesser.py
@@ -3,22 +3,27 @@ import random
 # Generate a random integer between 1 and 10
 random_number = random.randint(1, 10)
 print("I'm thinking of a number between 1-10!")
+
 guess = 0
+guess_counter = 0
 
 while guess != random_number:
 
     try:
         guess = int(input("What's your guess? "))
 
-    except ValueError:
+    except ValueError: #safetynet if the user inputs non-numeric character
         print("That's not a number bruh! Try again.")
         continue
 
+    guess_counter+= 1
+
     if guess > random_number:
-        print("Lower!")
+        print("It's lower!")
 
     elif guess < random_number:
-        print("higher")
+        print("The number is higher!")
 
     else:
-        print("You won!")
+        word = "guess" if guess_counter == 1 else "guesses"
+        print(f"You won in {guess_counter} {word}!")
