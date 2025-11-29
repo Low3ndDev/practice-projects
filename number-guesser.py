7b10f83dd8b10af830efa4d1d5a4fb9c038a7d10
Low3ndDev <johnmark.betes@gmail.com>
2025-11-29 10:43:27 +0800

    Add replay functionality with play again prompt

--- number-guesser.py
+++ number-guesser.py
@@ -1,29 +1,36 @@
 import random
 
+while True: 
+
 # Generate a random integer between 1 and 10
-random_number = random.randint(1, 10)
-print("I'm thinking of a number between 1-10!")
+    random_number = random.randint(1, 10)
+    print("I'm thinking of a number between 1-10!")
 
-guess = 0
-guess_counter = 0
+    guess = 0
+    guess_counter = 0
 
-while guess != random_number:
+    while guess != random_number:
 
-    try:
-        guess = int(input("What's your guess? "))
+        try:
+            guess = int(input("What's your guess? "))
 
-    except ValueError: #safetynet if the user inputs non-numeric character
-        print("That's not a number bruh! Try again.")
-        continue
+        except ValueError: #safetynet if the user inputs non-numeric character
+            print("That's not a number bruh! Try again.")
+            continue
 
-    guess_counter+= 1
+        guess_counter+= 1
 
-    if guess > random_number:
-        print("It's lower!")
+        if guess > random_number:
+            print("It's lower!")
 
-    elif guess < random_number:
-        print("The number is higher!")
+        elif guess < random_number:
+            print("The number is higher!")
 
-    else:
-        word = "guess" if guess_counter == 1 else "guesses"
-        print(f"You won in {guess_counter} {word}!")
+        else:
+            word = "guess" if guess_counter == 1 else "guesses"
+            print(f"You won in {guess_counter} {word}!")
+
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
